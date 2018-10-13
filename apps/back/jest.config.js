649d9c25ee86aab32f8d692d6f552995c0f87011
Fay <fayway@gmail.com>
2018-10-13 23:36:28 +0200

    init

---
+++ apps/back/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'back',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/back'
+};
---
+++ apps/front/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'front',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/front/'
+};
